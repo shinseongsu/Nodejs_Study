0fbda5d025aabc7a0b83eaf98e4df51f05f4d209
Shin seongsu <tlstjdtn321@naver.com>
2020-04-02 21:17:06 +0900

    mongoose 연결

---
+++ chapter6/learn-mongoose/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var commentsRouter = require('./routes/comments');
+var connect = require('./schemas');
+
+var app = express();
+connect();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
+
+module.exports = app;
